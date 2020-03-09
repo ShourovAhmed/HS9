0d61f42684fce4a33a6983dfcebeca2d6754c107
Shourov <shourov.ahmed@student.ap.be>
2020-03-09 09:50:48 +0100

    Add project files.

---
+++ Meetlat/Lat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meetlat
+{
+    class Lat
+    {
+        public int BeginLengte { private get; set; }
+        public int LengteInM
+        {
+            get;
+
+        }
+        public int LengteInCm { get; }
+        public int LengteInKm { get; }
+        public int LengteInVoet { get; }
+    }
+}
