ff0f83cda9fe7ea9bf226f3c222024fe069800ba
Shourov <shourov.ahmed@student.ap.be>
2020-04-16 20:19:01 +0200

    Pokémon klasse aangemaakt met (voorlopig) alle stats(propertys) en methode verhooglevel

---
+++ Pokémon/Pokémon.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokémon
+{
+    class Pokémon
+    {
+
+        //BASE STATS
+
+        private int hp_base;
+
+        public int HP_Base
+        {
+            get { return hp_base; }
+            set { hp_base = value; }
+        }
+
+        private int attack_base;
+
+        public int Attack_Base
+        {
+            get { return attack_base; }
+            set { attack_base = value; }
+        }
+
+        private int defense_base;
+
+        public int Defense_Base
+        {
+            get { return defense_base; }
+            set { defense_base = value; }
+        }
+
+        private int specialAttack_base;
+
+        public int SpecialAttack_Base
+        {
+            get { return specialAttack_base; }
+            set { specialAttack_base = value; }
+        }
+
+        private int specialDefense_base;
+
+        public int SpecialDefense_Base
+        {
+            get { return specialDefense_base; }
+            set { specialDefense_base = value; }
+        }
+
+        private int speed_base;
+
+        public int Speed_Base
+        {
+            get { return speed_base; }
+            set { speed_base = value; }
+        }
+
+        //Extra Stats
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Number { get; set; }
+
+        //READ-ONLY
+        public int Average
+        {
+            get
+            { return (HP_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefense_Base + Speed_Base) / 6; }
+        }
+        public int Total
+        {
+            get
+            { return (HP_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefense_Base + Speed_Base); }
+        }
+
+        //LEVEL BASED STATS
+        private int level;
+
+        public int Level
+        {
+            get { return level; }
+            private set { level = value; }
+        }
+
+        public int HP_Full
+        {
+            get
+            { return ((((HP_Base + 50) * Level) / 50) + 10); }
+        }
+
+        public int Attack_Full
+        {
+            get
+            {
+                return (((Attack_Base * Level) / 50) + 5);
+            }
+        }
+
+        public int Defense_Full
+        {
+            get
+            {
+                return (((Defense_Base * Level) / 50) + 5);
+            }
+        }
+
+        public int SpecialAttack_Full
+        {
+            get
+            {
+                return (((SpecialAttack_Base * Level) / 50) + 5);
+            }
+        }
+
+        public int SpecialDefense_Full
+        {
+            get
+            {
+                return (((SpecialDefense_Base * Level) / 50) + 5);
+            }
+        }
+
+        public int Speed_Full
+        {
+            get
+            {
+                return (((Speed_Base * Level) / 50) + 5);
+            }
+        }
+
+
+        //METHODEN
+        public void VerhoogLevel()
+        {
+            Level++;
+        }
+
+
+
+
+    }
+}
---
+++ Pokémon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pokémon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
